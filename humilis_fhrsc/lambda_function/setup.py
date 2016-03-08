7baa43f3c94288004b95eb33e2984ab961fe45d4
German Gomez-Herrero <german@innovativetravel.eu>
2016-03-08 18:55:52 +0100

    Fix installation bugs

--- humilis_fhrsc/lambda_function/setup.py
+++ humilis_fhrsc/lambda_function/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="humilis-firehose-resource",
-    version=metadata.version,
+    version="0.0.1",
     packages=find_packages(),
     include_package_data=True,
     # We often need the latest version of boto3 so we include it as a req
